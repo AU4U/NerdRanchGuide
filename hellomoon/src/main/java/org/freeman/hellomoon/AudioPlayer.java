727555954e9d9cd36624b3f44f52f33f039543c1
freemanliu <381208198@qq.com>
2017-01-22 17:40:21 +0800

    hello moon init

---
+++ hellomoon/src/main/java/org/freeman/hellomoon/AudioPlayer.java
@@ -0,0 +1,30 @@
+package org.freeman.hellomoon;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+
+/**
+ * Created by freemanliu on 2017/1/22.
+ */
+public class AudioPlayer {
+
+    private MediaPlayer mPlayer;
+
+    public void stop() {
+        if (mPlayer != null) {
+            mPlayer.release();
+            mPlayer = null;
+        }
+    }
+
+    public void play(Context context) {
+        stop();
+        mPlayer = MediaPlayer.create(context,R.raw.one_small_step);
+
+        mPlayer.start();
+    }
+
+    public boolean isPlaying() {
+        return mPlayer != null;
+    }
+}
